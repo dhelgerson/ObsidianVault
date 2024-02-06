9659d34189d3a2ed49d1a21f3dd52763ff16c064
dhelgerson <tah568@msstate.edu>
2024-02-06 11:30:46 -0600

    vault backup: 2024-02-06 11:30:46

--- dataStruc/StacksAndQueues.cpp
+++ dataStruc/StacksAndQueues.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 // codium notes for stacks and queues
+// stacks are akin to loading a pez dispenser 1 * 1
+// stacks are a last-in first-out (LIFO)
+// you only ever interact w/ the one on top
 
 int main(){
 
