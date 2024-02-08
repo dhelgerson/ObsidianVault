e8960a12f3a3998cd97d0afdc3b48c5b5ad86bab
dhelgerson <tah568@msstate.edu>
2024-02-07 18:22:54 -0600

    yay, we did things

---
+++ dataStruc/LinkedList.h
@@ -0,0 +1,64 @@
+#include "Node.h"
+// #include "LinkedList.cpp"
+#include <iostream>
+
+class LinkedList{
+    private:
+        Node *head;
+        Node *tail;
+        int len;
+    public:
+        LinkedList():head(nullptr),tail(nullptr){}
+        ~LinkedList();
+
+        void prepend(int payload);
+        void append(int payload);
+        void remove(int match);
+        void display(std::ostream &output);
+};
+
+LinkedList::~LinkedList(){
+    head->cascade();
+    delete head;
+}
+void LinkedList::prepend(int payload){
+    if (head == nullptr){
+        head = new Node(payload);
+        tail = head;
+    }
+    else{
+        Node *tmp = head;
+        head = new Node(payload);
+        head->next = tmp;
+    }
+}
+void LinkedList::append(int payload){
+    if (head == nullptr){
+        head = new Node(payload);
+        tail = head;
+    }
+    else {
+        Node *tmp = tail;
+        tail = new Node(payload);
+        tmp->next = tail;
+    }
+}
+void LinkedList::display(std::ostream &output){
+    Node *tmp = head;
+    while (tmp) {
+        output << tmp->payload << ' ';
+        tmp = tmp->next;
+    }
+    output << std::endl;
+}
+void LinkedList::remove(int key){
+    Node *tmp = head;
+    while (tmp){
+        if (tmp->next->payload == key){
+            break;
+        }
+    }
+    Node *del = tmp->next;
+    tmp->next = del->next;
+    delete del;
+}
--- dataStruc/StacksAndQueues.cpp
+++ dataStruc/StacksAndQueues.cpp
@@ -9,18 +9,7 @@ using namespace std;
 // stacks can be implemented with various data models. linked lists work well and will
 // be used in this case.
 
-class Node{
-    // here we define the basic legos of the data structure. these will be dynamically allocated
-    // by our next class
-    public:
-        int payload;
-        Node *next;
-
-        Node(): payload(0),next(nullptr) {}
-        Node(int payload):payload(payload),next(nullptr) {}
-        Node(int payload,Node *next):payload(payload), next(next) {}
-};
-
+#include "Node.h"
 class Stack{
     // here's where we do the real work, we dynamically allocate nodes and keep track of them w/ pointers
     private:
