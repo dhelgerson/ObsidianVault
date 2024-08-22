d68c3cfc09bf895696bb88e20eb20ca18c0b4c09
Drew Helgerson <tah568@msstate.edu>
2024-08-22 16:44:13 -0500

    vault backup: 2024-08-22 16:44:13

---
+++ systems/code/testing.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char *stl = "Hello world";
+    char *nstl;
+
+    strcpy(stl,nstl);
+    strcmp(stl,nstl);
+
+    printf("%s\n",stl);
+
+    return 0;
+}
