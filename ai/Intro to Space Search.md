380f683030c5669da8646cff0529d6c0c395b300
Drew Helgerson <tah568@msstate.edu>
2024-09-04 08:16:40 -0500

    vault backup: 2024-09-04 08:16:40

--- ai/Intro to Space Search.md
+++ ai/Intro to Space Search.md
@@ -1,1 +1,3 @@
 08/29/24
+
+so how exactly is this supposed to work?
---
+++ sys/code/program2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{
+    int fd;
+    void *data = malloc(20);
+    fd = open('raw.dat',O_RDONLY);
+    read(fd,data,20);
+
+    return 0;
+}
