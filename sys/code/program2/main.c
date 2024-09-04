dc7b1c73530ee79a6ff1a344398aa5de81860482
Drew Helgerson <tah568@msstate.edu>
2024-09-04 14:57:34 -0500

    vault backup: 2024-09-04 14:57:34

--- sys/code/program2/main.c
+++ sys/code/program2/main.c
@@ -8,20 +8,20 @@
 
 int main()
 {
-    int fd;
+    int in,out;
     short s;
     double val;
     unsigned char bytes[20];
 
-    fd = open("raw.dat",O_RDONLY);
-    read(fd,bytes,20);
+    in = open("raw.dat",O_RDONLY);
+    // read(in,bytes,20);
 
-    for(int i=2; i<20;i+=2) {
-        s = ((short)bytes[i+1] << 8) | bytes[i];
-        val = s / 32768;
-        printf("%i ",i);
-        printf("%hi %lf\n",s,val);
-    }
+    while(read(in,bytes,20) > 0)
+        for(int i=2; i<20;i+=2) {
+            s = ((short)bytes[i+1] << 8) | bytes[i];
+            val = s / 32768.0;
+            printf("%i %hi %lf\n",i/2,s,val);
+        }
 
     return 0;
 }
