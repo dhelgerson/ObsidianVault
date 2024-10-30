37cbe7fc6c57b71ce705eb9c7c27160b7ea24fe9
Drew Helgerson <tah568@msstate.edu>
2024-10-29 21:53:02 -0500

    working commit.asm

--- org/nand2tetris/project/05/fill/Fill.asm
+++ org/nand2tetris/project/05/fill/Fill.asm
@@ -11,4 +11,116 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
+@SCREEN
+D=A
+@screenStart
+M=D
+
+// Selects KBD (24576) and creates a variable
+// based on the ending location of the screen
+// NOTE: End of screen is 1 register before the KBD.
+@KBD
+D=A-1
+@screenEnd
+M=D
+
+
 // Put your code here.
+
+// variable last keeb input
+@KBLast
+M=0
+
+@While
+0;JMP
+
+(FILL_INIT)
+    @screenStart
+    D=M 
+    @currentLoc
+    M=D
+
+// Beginning of FILL loop
+(FILL)
+    // Sets our current location in memory 
+    // based on our location variable
+    // NOTE: This is pointer notation
+    @currentLoc
+    A=M
+    M=-1 // fills the entire register
+
+    // Checks if our current location is
+    // at the end of the screen. Jumps to
+    // CHECK_KEYBOARD if so
+    @currentLoc
+    D=M
+    @screenEnd
+    D=M-D
+    @While
+    D;JLE
+
+    // Increments our current location
+    // in the screen and restarts the 
+    // FILL loop
+	@currentLoc
+	M=M+1
+	@FILL
+	0;JMP
+
+(CLEAR_INIT)
+    @screenStart
+    D=M 
+    @currentLoc
+    M=D
+
+(CLEAR)
+    @currentLoc
+    A=M
+    M=0 // clears the entire register
+
+    // Checks if our current location is
+    // at the end of the screen. Jumps to
+    // CHECK_KEYBOARD if so
+    @currentLoc
+    D=M
+    @screenEnd
+    D=M-D
+    @While
+    D;JLE
+
+    // Increments our current location
+    // in the screen and restarts the 
+    // CLEAR loop
+    @currentLoc
+    M=M+1
+    @CLEAR
+    0;JMP
+    
+
+(While)
+// while true:
+//     read keyboard
+    @KBD
+    D=M    
+
+//     if keeb state matches last keeb state:
+//         continue
+    @KBLast
+    D=D-M
+    @While
+    D;JEQ
+
+//     if keeb key false:
+//         black screen function
+    @KBD
+    D=M
+    @KBLast
+    M=D
+    @CLEAR_INIT
+    D;JEQ
+
+//     else (keeb key true):
+//         white screen function
+    @FILL_INIT
+    D;JGT
+//         continue
